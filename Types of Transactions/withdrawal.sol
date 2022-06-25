5967280aa1cf4a564dff357e9294edce1c3ea452
HaigEmirzian <92221924+HaigEmirzian@users.noreply.github.com>
2022-06-24 20:38:23 -0400

    Update withdrawal.sol

--- Types of Transactions/withdrawal.sol
+++ Types of Transactions/withdrawal.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
 
+// Purpose: To withdraw cryptocurrency from a crypto broker
+
+pragma solidity ^0.8.15;
+
+contract withdrawal {
+    // instantiating the broker
+    address broker;
+
+    // only the broker can transfer money for the purposes of this SC
+    constructor(){
+        broker = msg.sender;
+    }
+
+    // attributes that verify the customer
+    struct customer {
+        string name;
+        address walletAddress;
+        uint amount;
+    }
+
+    
+}
