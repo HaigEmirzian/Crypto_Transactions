bc64481f47ead7d1150a52fd2f14c5027f912df2
HaigEmirzian <92221924+HaigEmirzian@users.noreply.github.com>
2022-07-06 22:54:28 -0400

    Update withdrawal.sol

--- Types of Transactions/withdrawal.sol
+++ Types of Transactions/withdrawal.sol
@@ -24,29 +24,27 @@ contract withdrawal {
     // instantiates the user information within an array
     Customer[] public customer;
 
-    // security: Ensures the sender for the withdrawal is the broker
-    modifier onlyBroker() {
-        require(msg.sender == broker, "For a withdrawal, the broker needs to give money");
-        _;
-    }
-
     // adds a customer to the array of customers
-    function addCustomer(string memory firstName, string memory lastName, address payable walletAddress, uint amount) public onlyBroker {
+    function addCustomer(string memory firstName, string memory lastName, address payable walletAddress, uint amount) public {
         customer.push(Customer(firstName, lastName, walletAddress, amount));
         }
 
-    function getIndex(address walletAddress) view private returns(uint) {
-        for(uint i = 0; i < customer.length; i++) {
-            if (customer[i].walletAddress == walletAddress) {
-                return i;
-            }
-        }
-        return 999;
+    // find balance
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
     }
 
-    function withdraw(address payable walletAddress) payable public {
-        uint i = getIndex(walletAddress);
-        require(msg.sender == customer[i].walletAddress, "You must be the customer to withdraw");
-        customer[i].walletAddress.transfer(customer[i].amount);
+    // allows the user to withdraw from a broker
+    function withdrawFrom(address walletAddress) payable public {
+        addToCustomersBalance(walletAddress);
+    }
+
+    // updates the customer's balance
+    function addToCustomersBalance(address walletAddress) private {
+        for (uint i = 0; i < customer.length; i++){
+            if (customer[i].walletAddress == walletAddress){
+                customer[i].amount += msg.value;
+            }
+        }
     }
 }
